f2e62223dc3e9ff83109ecf102dadef012e12d7d
MuseGr <nemanja.stefanovic3105@gmail.com>
2022-11-28 01:53:49 +0100

    added sign in component as well as firebase db

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Navigation from './routes/navigation/navigation.component';
-
+import SignIn from './routes/sign-in/sing-in.componetn';
 import Home from "./routes/home/home.component";
 
 
@@ -13,8 +13,9 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={ <Navigation /> }>
-        <Route index element={ <Home /> }/> {/* Index ubacuje home u outlet gavnog dira */}
+        <Route index element={ <Home /> }/> {/* Index ubacuje home u outlet glavnog dira */}
         <Route path='shop' element={ <Shop /> }/>
+        <Route path='signIn' element={ <SignIn /> }/>
       </Route>
     </Routes>
   ) 
